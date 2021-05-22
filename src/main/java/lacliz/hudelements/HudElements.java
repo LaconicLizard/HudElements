04db543f002129531f849344336a80df2efc204c
LaconicLizard <laconiclizard976@protonmail.com>
2021-05-22 01:35:00 -0700

    Created mod, added todo list

---
+++ src/main/java/lacliz/hudelements/HudElements.java
@@ -0,0 +1,10 @@
+package lacliz.hudelements;
+
+import net.fabricmc.api.ModInitializer;
+
+public class HudElements implements ModInitializer {
+
+    @Override public void onInitialize() {
+    }
+
+}
